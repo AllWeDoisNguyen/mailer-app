649948b5b2f409cfab71b2a2bff921ac9d8bc443
Christina Nguyen <christina.nguyen.cn@gmail.com>
2016-05-19 15:32:21 -0400

    Create follow up email worker

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class UserMailerTest < ActionMailer::TestCase
   test "follow_up" do
     target_email = "me@example.com"
-    mail = UserMailer.follow_up(target_email).deliver_now
+    email = UserMailer.follow_up(target_email).deliver_now
 
     assert_not ActionMailer::Base.deliveries.empty?
     # assert_equal [target_email], email.to
 
     # assert_equal "Follow up", mail.subject
-    assert_equal [target_email], mail.to
-    assert_equal ["no-reply@example.com"], mail.from
-    assert_equal "How are things going?", mail.subject
+    assert_equal [target_email], email.to
+    assert_equal ["no-reply@example.com"], email.from
+    assert_equal "How are things going?", email.subject
     # assert_match "Hi", mail.body.encoded
-    assert_match "Twitter and Facebook", mail.body.encoded
+    assert_match "Twitter and Facebook", email.body.encoded
   end
 
 end
